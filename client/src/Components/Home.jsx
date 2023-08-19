1380ed36915f4c16143f9144009721ecb24380a3
Abditive <andreina.orte@gmail.com>
2023-08-19 14:23:56 +1000

    openai fix

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_API_KEY,
+  dangerouslyAllowBrowser: true,
 });
 
 function Home(props) {
